b6cc0fac677e617550ecd2d2d86b1cbdce7dab07
Oshernan <o.racso11@gmail.com>
2016-12-06 21:17:02 +0000

    Formulario de registro

---
+++ Gateway/src/main/java/es/uc3m/tiw/thymeleaf/configuration/ThymeleafExtension.java
@@ -0,0 +1,26 @@
+package es.uc3m.tiw.thymeleaf.configuration;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+@Configuration
+public class ThymeleafExtension {
+
+    @Autowired
+    private SpringTemplateEngine templateEngine;
+
+    @PostConstruct
+    public void extension() {
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setPrefix("\templates");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(templateEngine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+        templateEngine.addTemplateResolver(resolver);
+    }
+}
