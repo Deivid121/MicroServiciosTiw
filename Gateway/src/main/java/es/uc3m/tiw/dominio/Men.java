cd1f9a831bf58343d9259f59e7ddb63863e648c5
rborges <borgesrodri24@gmail.com>
2016-12-17 09:39:09 +0000

    men

---
+++ Gateway/src/main/java/es/uc3m/tiw/dominio/Men.java
@@ -0,0 +1,18 @@
+package es.uc3m.tiw.dominio;
+
+public class Men {
+	private String mensaje;
+	public Men() {
+	}
+	public Men(String mensaje){
+		this.mensaje = mensaje;
+	}
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+
+}
